b16496e4d3faaa39a0ac0944ca765bce9e84f34c
nguyencongduc <nguyencongduc18THD@gmail.com>
2024-07-31 16:55:08 +0700

    first commit

---
+++ api/controllers/StockController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Dtos.Stock;
+using api.Mappers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController :ControllerBase
+    { private readonly ApplicationDBContext _context;
+        public StockController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stocks = this._context.Stocks.ToList().Select(s=>s.ToStockDto());
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult  GetById([FromRoute] int id)
+        {
+            var stock = _context.Stocks.Find(id);
+
+            if(stock == null )
+            {
+                return NotFound() ;
+            }
+            return Ok(stock.ToStockDto());
+        }
+        
+        [HttpPost]
+        public IActionResult Create([FromBody] CreateStockRequestDto stockDto)
+        {
+            var stockModel = stockDto.ToStockFromCreateDTO();
+            _context.Stocks.Add(stockModel);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetById), new { id =stockModel.Id}, stockModel.ToStockDto());
+        }
+        
+    }
+}
