7f5f6059bb570ea6678f37cc6129a8af332580b4
nguyencongduc <nguyencongduc18THD@gmail.com>
2024-07-31 17:19:53 +0700

    update stock

--- api/controllers/StockController.cs
+++ api/controllers/StockController.cs
@@ -45,5 +45,27 @@ namespace api.controllers
             return CreatedAtAction(nameof(GetById), new { id =stockModel.Id}, stockModel.ToStockDto());
         }
         
+        [HttpPut]
+        [Route("{id}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] UpdateStockRequestDto stockDto)
+        {
+           var stockModel = _context.Stocks.FirstOrDefault(s => s.Id == id);
+
+            if(stockModel == null)
+            {
+                return NotFound();
+            }
+            stockModel.Symbol = stockDto.Symbol;
+            stockModel.CompanyName = stockDto.CompanyName;
+            stockModel.Purchase = stockDto.Purchase;
+            stockModel.LastDiv = stockDto.LastDiv;
+            stockModel.Industry = stockDto.Industry;
+            stockModel.MarketCap = stockDto.MarketCap;
+
+            _context.SaveChanges();
+
+            return Ok(stockModel.ToStockDto);
+        }
+
     }
 }
